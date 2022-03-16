28951c0d5ce22e964417b6a2ae5879141a692b3c
yo16 <xxyo1xx@gmail.com>
2022-03-17 01:10:44 +0900

    超基礎

--- src/scatter-3d.js
+++ src/scatter-3d.js
@@ -14,6 +14,9 @@ function draw_three(){
     // 初期化
     initialize();
 
+    // 基本要素を追加
+    add_basic_elements();
+
     // 要素追加
     add_elements();
 
@@ -21,7 +24,9 @@ function draw_three(){
 
 // 初期化
 function initialize(){
+    // シーン
     scene = new THREE.Scene();
+    //scene.background = new THREE.Color( 0xffffff );
 
     // カメラ
     camera = new THREE.PerspectiveCamera( 60, width / height );     // 視野角, アスペクト比, near, far
@@ -29,7 +34,12 @@ function initialize(){
     camera.lookAt(new THREE.Vector3(0, 0, 0));  // カメラの向き
 
     // ライト
-    light = new THREE.DirectionalLight(0xFFFFFF, 1);    // 色, 強さ
+    // 環境光源
+    ambient_light = new THREE.AmbientLight(0xFFFFFF, 0.5);
+    scene.add(ambient_light);
+    // 平行光源
+    light = new THREE.DirectionalLight(0xFFFFFF, 0.5);    // 色, 強さ
+    light.position.set(0, 1, 1);
     scene.add(light);
 
     // レンダラーを設定
@@ -41,10 +51,33 @@ function initialize(){
     document.body.appendChild( renderer.domElement );
 }
 
+
+// 基本要素を追加
+function add_basic_elements(){
+    // 底面
+    plane_geo = new THREE.PlaneGeometry(10, 10, 1, 1);  // width, height, widthSegments, heightSegments
+    plane_mat = new THREE.MeshLambertMaterial( { color: 0x666666 } );
+    /* 透過
+    plane_mat.opacity = 0.2;
+    plane_mat.transparent = true;   // 透過
+    plane_mat.depthTest = false;    // 陰面処理
+    */
+    plane = new THREE.Mesh( plane_geo, plane_mat );
+    plane.position.set(5, 0, 5);
+    plane.rotation.x = -0.5 * Math.PI;
+    scene.add(plane);
+
+    // 軸    
+    const axes = new THREE.AxesHelper(12);
+    scene.add(axes);
+}
+
+
+// 要素を追加
 function add_elements(){
     // box
     let geo1 = new THREE.BoxGeometry(1, 1, 1);  // x-width, y-width, z-width
-    let material_basic = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    let material_basic = new THREE.MeshLambertMaterial( { color: 0x00aa00 } );
     box = new THREE.Mesh( geo1, material_basic );
     box.position.set(0.5, 0.5, 0.5);
     scene.add( box );
@@ -58,14 +91,15 @@ function add_elements(){
     scene.add( donut );
     elms.push( donut );
 
-
 }
 
 // アニメーション定義
 function animate() {
+    /*
     for(let i=0; i<elms.length; i++){
         elms[i].rotation.y += 0.01;
     }
+    */
     //e1.rotation.y += 0.01;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
