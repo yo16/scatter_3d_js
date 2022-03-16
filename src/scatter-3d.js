14dd7315d42090f9a35177d363757958c8934c73
yo16 <xxyo1xx@gmail.com>
2022-03-17 00:38:03 +0900

    add donut

--- src/scatter-3d.js
+++ src/scatter-3d.js
@@ -4,8 +4,8 @@ var camera = null;
 var light = null;
 var renderer = null;
 
-var width = 500;
-var height = 500;
+var width = 640;
+var height = 480;
 
 //var cube = null;
 var elms = [];
@@ -14,6 +14,7 @@ function draw_three(){
     // 初期化
     initialize();
 
+    // 要素追加
     add_elements();
 
 }
@@ -21,11 +22,14 @@ function draw_three(){
 // 初期化
 function initialize(){
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-    camera.position.z = 5;
+
+    // カメラ
+    camera = new THREE.PerspectiveCamera( 60, width / height );     // 視野角, アスペクト比, near, far
+    camera.position.set(15, 15, 15);    // 位置
+    camera.lookAt(new THREE.Vector3(0, 0, 0));  // カメラの向き
 
     // ライト
-    light = new THREE.DirectionalLight(0xFFFFFF, 1);
+    light = new THREE.DirectionalLight(0xFFFFFF, 1);    // 色, 強さ
     scene.add(light);
 
     // レンダラーを設定
@@ -38,21 +42,26 @@ function initialize(){
 }
 
 function add_elements(){
-    let geometry = new THREE.BoxGeometry();
+    // box
+    let geo1 = new THREE.BoxGeometry(1, 1, 1);  // x-width, y-width, z-width
     let material_basic = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    box = new THREE.Mesh( geometry, material_basic );
+    box = new THREE.Mesh( geo1, material_basic );
+    box.position.set(0.5, 0.5, 0.5);
     scene.add( box );
     elms.push( box );
-    
-    /*
-    let material_lamber = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
-    cube = new THREE.Mesh( geometry, material_lamber );
-    scene.add( cube );
-    */
+
+    // ドーナッツ
+    let geo2 = new THREE.TorusGeometry(2, 1, 30, 90);  // r, tube-r, radialSegments, tubalarSegments
+    let material_lamber = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+    donut = new THREE.Mesh( geo2, material_lamber );
+    donut.position.set(5, 5, 5);
+    scene.add( donut );
+    elms.push( donut );
 
 
 }
 
+// アニメーション定義
 function animate() {
     for(let i=0; i<elms.length; i++){
         elms[i].rotation.y += 0.01;
